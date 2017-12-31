4c840ade2bb53626280940ab906db51656301e51
Baurzhan Ismagulov <ibr@ilbers.de>
2017-12-31 10:28:24 +0100

    ci_build.sh: Add custom build dir support
    ci_build.sh uses a hard-coded absolute build dir path. This change
    allows specifying the build dir on the command line.
    
    Signed-off-by: Baurzhan Ismagulov <ibr@ilbers.de>

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -5,21 +5,40 @@
 # Alexander Smirnov <asmirnov@ilbers.de>
 # Copyright (c) 2016-2017 ilbers GmbH
 
+ES_OK=0
+ES_SYNTAX=1
+
 # Export $PATH to use 'parted' tool
 export PATH=$PATH:/sbin
 
 # Get parameters from the command line
-WORKSPACE=$1
-GIT_COMMIT=$2
+# TODO: Unify
+case $# in
+1)
+    build_dir="$1"
+    ;;
+2)
+    WORKSPACE="$1"
+    GIT_COMMIT="$2"
+    # TODO: Don't hardcode absolute paths
+    build_dir="/build/$WORKSPACE/$GIT_COMMIT"
+    ;;
+*)
+    echo "$0: Wrong number of arguments" >&2
+    echo "Usage: $0 BUILD_DIR" >&2
+    exit $ES_SYNTAX
+    ;;
+esac
 
 # Go to Isar root
 cd $(dirname $0)/..
 
 # Setup build folder for current revision
-if [ ! -d /build/$WORKSPACE/$GIT_COMMIT ]; then
-        mkdir -p /build/$WORKSPACE/$GIT_COMMIT
+# TODO: Why create the dir if isar-init-build-env should do that anyway?
+if [ ! -d "$build_dir" ]; then
+        mkdir -p "$build_dir"
 fi
-. ./isar-init-build-env /build/$WORKSPACE/$GIT_COMMIT
+. ./isar-init-build-env "$build_dir"
 
 # Start build for all possible configurations
 bitbake \
@@ -31,3 +50,5 @@ bitbake \
         multiconfig:qemuamd64-jessie:isar-image-base \
         multiconfig:qemuamd64-stretch:isar-image-base \
         multiconfig:rpi-jessie:isar-image-base
+
+exit $ES_OK
